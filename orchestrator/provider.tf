a033e6511d009a3a3ffdbc8e417202e5d27ad735
cosmindev <cosmin.tudor82@gmail.com>
2023-12-08 08:25:00 +0200

    first commit

---
+++ orchestrator/provider.tf
@@ -0,0 +1,29 @@
+# ####################################################################################################### #
+# Copyright (c) 2023 Oracle and/or its affiliates,  All rights reserved.                                  #
+# Licensed under the Universal Permissive License v 1.0 as shown at https: //oss.oracle.com/licenses/upl. #
+# Author: Cosmin Tudor                                                                                    #
+# Author email: cosmin.tudor@oracle.com                                                                   #
+# Last Modified: Thu Nov 16 2023                                                                          #
+# Modified by: Cosmin Tudor, email: cosmin.tudor@oracle.com                                               #
+# ####################################################################################################### #
+
+provider "oci" {
+  region               = var.home_region
+  tenancy_ocid         = var.tenancy_ocid
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
+  private_key_password = var.private_key_password
+}
+
+terraform {
+  required_version = "< 1.3.0"
+  required_providers {
+    oci = {
+      source                = "oracle/oci"
+      version               = "<= 5.16.0"
+      configuration_aliases = [oci]
+    }
+  }
+  experiments = [module_variable_optional_attrs]
+}
