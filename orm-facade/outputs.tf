495655fd502ab7f7252ad85ba6af0b9e869e9735
cosmindev <cosmin.tudor82@gmail.com>
2023-12-20 15:01:20 +0200

    Rename module

--- orm-facade/outputs.tf
+++ orm-facade/outputs.tf
@@ -3,17 +3,17 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https: //oss.oracle.com/licenses/upl. #
 # Author: Cosmin Tudor                                                                                    #
 # Author email: cosmin.tudor@oracle.com                                                                   #
-# Last Modified: Tue Dec 19 2023                                                                          #
+# Last Modified: Wed Dec 20 2023                                                                          #
 # Modified by: Cosmin Tudor, email: cosmin.tudor@oracle.com                                               #
 # ####################################################################################################### #
 
 output "provisioned_identity_resources" {
   description = "Provisioned identity resources"
-  value       = module.terraform-oci-open-lz.provisioned_identity_resources
+  value       = module.terraform-oci-cis-modules-orchestrator.provisioned_identity_resources
 }
 
 output "provisioned_networking_resources" {
   description = "Provisioned networking resources"
-  value       = module.terraform-oci-open-lz.provisioned_networking_resources
+  value       = module.terraform-oci-cis-modules-orchestrator.provisioned_networking_resources
 }
 
